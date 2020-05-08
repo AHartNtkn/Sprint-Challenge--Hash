a35e9c7f6690ba52ab4e1e8b99cd6987e8cc385b
AHartNtkn <AHartNtkn@users.noreply.github.com>
2020-05-08 08:01:46 -0700

    Ex 1 done.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,12 @@
 def get_indices_of_item_weights(weights, length, limit):
+    dict = {}
 
-    """
-    YOUR CODE HERE
-    """
+    for i, w in enumerate(weights):
+        dict[w] = i
+
+    for i, w in enumerate(weights):
+        r = dict.get(limit - w, None)
+        if r:
+            return [ r, i ]
 
     return None
